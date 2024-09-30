27482b52e6ab20c482267bc759ac31ca88dbfe04
Rick Wright <rickiswright@live.com>
2024-09-30 07:10:57 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MasterChildFormsSuiteFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
